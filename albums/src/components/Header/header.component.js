9b70c474efafe90268277666927acb37f9a1dd2e
Lenya Hope <lehone4hope@gmail.com>
2019-05-18 14:51:23 +0100

    albums: Make Header component

---
+++ albums/src/components/Header/header.component.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Header = () => {
+    return (
+        <View style={ styles.headerTextContainer }>
+            <Text style={ styles.headerText }>Albums!</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    headerTextContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f8f8f8',
+        height: 60,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        elevation: 5
+    }, headerText: {
+        fontSize: 24,
+    }
+});
+
+export default Header;
