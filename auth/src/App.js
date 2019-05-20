5ab8d91eecdf94357f1e06d77ab12dd73ffc2b30
Lenya Hope <lehone4hope@gmail.com>
2019-05-20 17:07:44 +0100

    auth: started new project:

---
+++ auth/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    state = {  }
+    render() {
+        return (
+            <View>
+                <Text>My App</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
